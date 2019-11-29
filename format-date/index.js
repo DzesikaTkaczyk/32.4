ee0804af0734744f5ad8abb2cad4b55156017aa1
DzesikaTkaczyk <50298851+DzesikaTkaczyk@users.noreply.github.com>
2019-11-29 09:04:01 +0100

    Update index.js

--- format-date/index.js
+++ format-date/index.js
@@ -1,29 +1,29 @@
 const formatDate = (timeInSeconds) => {
-	let timeString = ''
-	let hourString = ''
-	let minuteString = ''
-	let secondsString = ''
+	let timeString = '';
+	let hourString = '';
+	let minuteString = '';
+	let secondsString = '';
 	if (timeInSeconds >= 3600) {
 		hourString += Math.floor(timeInSeconds / 3600) + 'h'
-	}
+	};
 	if ((timeInSeconds % 3600) >= 60 && (timeInSeconds % 3600) !== 0 ) {
 		minuteString += Math.floor((timeInSeconds % 3600) / 60) + 'm'
-	}
+	};
 	if (((timeInSeconds % 3600) % 60) < 60 && ((timeInSeconds % 3600) % 60) !== 0) {
 		secondsString += ((timeInSeconds % 3600) % 60) + 's'
-	}
+	};
 	if (hourString !== '' && (minuteString !== '' || secondsString !== '')){
 		hourString += ' '
-	}
+	};
 	if (minuteString !== '' && secondsString !== ''){
 		minuteString += ' '
-	}
+	};
 	timeString = hourString + minuteString + secondsString
 	if (timeString === '') {
 		timeString = '0s'
-	}
-	return timeString
+	};
+	return timeString;
 
-}
+};
 
 module.exports = formatDate;
