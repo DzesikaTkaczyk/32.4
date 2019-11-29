fa492f231de4fe1044eb8991ee99a064b08ab2c0
DzesikaTkaczyk <50298851+DzesikaTkaczyk@users.noreply.github.com>
2019-11-29 19:46:34 +0100

    Update index.js

--- format-date/index.js
+++ format-date/index.js
@@ -7,20 +7,20 @@ const formatDate = (timeInSeconds) => {
 		hourString += Math.floor(timeInSeconds / 3600) + 'h'
 	};
 	if ((timeInSeconds % 3600) >= 60 && (timeInSeconds % 3600) !== 0 ) {
-		minuteString += Math.floor((timeInSeconds % 3600) / 60) + 'm'
+		minuteString += Math.floor((timeInSeconds % 3600) / 60) + 'm';
 	};
 	if (((timeInSeconds % 3600) % 60) < 60 && ((timeInSeconds % 3600) % 60) !== 0) {
-		secondsString += ((timeInSeconds % 3600) % 60) + 's'
+		secondsString += ((timeInSeconds % 3600) % 60) + 's';
 	};
 	if (hourString !== '' && (minuteString !== '' || secondsString !== '')){
-		hourString += ' '
+		hourString += ' ';
 	};
 	if (minuteString !== '' && secondsString !== ''){
-		minuteString += ' '
+		minuteString += ' ';
 	};
-	timeString = hourString + minuteString + secondsString
+	timeString = hourString + minuteString + secondsString;
 	if (timeString === '') {
-		timeString = '0s'
+		timeString = '0s';
 	};
 	return timeString;
 
